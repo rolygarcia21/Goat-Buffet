934365b8f037ed95a539ed6547f63daf14bdf42b
rolygarcia21 <47752832+rolygarcia21@users.noreply.github.com>
2020-04-25 14:51:20 -0400

    Initial commit
    yeehaw

--- Assets/Farmer.cs
+++ Assets/Farmer.cs
@@ -22,8 +22,6 @@ public class Farmer : MonoBehaviour
     void Update()
     {
     	goatPos = new Vector3(goatObject.transform.position.x, goatObject.transform.position.y, goatObject.transform.position.z);
-
-
         transform.position = Vector3.MoveTowards(transform.position, goatPos, 0.15f);
     }
 }
